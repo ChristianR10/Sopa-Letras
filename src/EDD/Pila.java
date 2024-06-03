ee301a4bbf12ea5dbedc0004c44fa0f56e096ff4
RojasA2005 <155595751+RojasA2005@users.noreply.github.com>
2024-06-02 22:05:39 -0400

    DFS
    Algoritmo de búsqueda DFS funcionando correctamente.

---
+++ src/EDD/Pila.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EDD;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cgrc1
+ */
+public class Pila {
+    private Nodo first;
+    private int size;
+    
+    public Pila (){
+        this.first = null;
+        this.size = 0;
+    }
+    
+    public boolean vacia (){
+    return first == null;
+    }
+    
+    public void apilar (String X){
+        Nodo nodo = new Nodo (X);
+        if (this.vacia()){
+            this.first = nodo;
+            this.size ++;
+        }
+        else{
+            nodo.setNext(first);
+            first = nodo;
+            size ++;
+        }
+    }
+    
+    public void desapilar (){
+        if(!vacia()){
+            first = first.getNext();
+            size --;
+        }
+    }
+    
+    public void apilarPalabraInv (String S){
+        String [] parts = S.split("");
+        for (int i=0; i<S.length();i++){
+            apilar(parts[S.length()-1-i]);
+        }
+    }
+    
+    public boolean encontrado (String X){
+        Nodo aux = this.first;
+        boolean z = false;
+        for (int i = 0; i<size;i++){
+            if (aux.getDato().equals(X)){
+                z = true;
+                break;
+            }
+            aux = aux.getNext();
+        }
+        return z;
+    }
+    
+    public void VaciarPila(){
+        first = null;
+        size = 0;
+    }
+    
+    public void imprimir (){
+        String cadena = "";
+        while (!vacia()){
+            cadena += first.getDato()+"-";
+            desapilar();
+        }
+        JOptionPane.showMessageDialog(null, cadena);
+    }
+    
+    public void imprimirInv (){
+        String cadena = "";
+        while (!vacia()){
+            cadena = "-" + first.getDato() + cadena;
+            desapilar();
+        }
+        JOptionPane.showMessageDialog(null, cadena);
+    }
+    
+    //getter and setter
+    public Nodo getFirst() {
+        return first;
+    }
+
+    public void setFirst(Nodo first) {
+        this.first = first;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+      
+}
--- src/Sopa_Letras/SopaLetras.java
+++ src/Sopa_Letras/SopaLetras.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -89,3 +90,148 @@ public class SopaLetras {
         
 }
 
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sopa_Letras;
+
+import EDD.Pila;
+import EDD.Cola;
+import Grafo.Arista;
+import Grafo.Grafo;
+import Grafo.Vertice;
+
+/**
+ *
+ * @author cgrc1
+ */
+public class SopaLetras {
+        char [][] LetraSopa; //Matriz usando las letras
+        int [][] PosicioneSopa; //Matriz usando el numero de vertice
+        int numCol; //Nro. Columnas de la matriz
+        int numFilas; //Nro. Filas de la matriz
+        Grafo grafo;
+
+    public SopaLetras(int x, int y) {
+        //Indica tamano de la sopa de letras (lo hice general, en el proyecto usaremos 4x4)
+        this.LetraSopa = new char [x][y];
+        this.PosicioneSopa = new int [x][y];
+        this.numCol = y;
+        this.numFilas = x;
+        this.grafo = new Grafo();
+    }
+            
+    public void armarSopa (String cadena){
+        cadena = cadena.toUpperCase();
+        String [] parts = cadena.split(", ");
+        crearVertices(parts, grafo);
+        crearAristas (grafo);
+    }
+    
+    public void crearVertices (String [] parts, Grafo grafo){
+        //Crear Vertices (Prueba)
+        int z = 0;
+        for (int i=0;i<numFilas;i++){
+            for (int j=0;j<numCol;j++){
+                LetraSopa [i][j] = parts[z].charAt(0); 
+                PosicioneSopa [i][j] = 4*(i)+(j);
+                grafo.agregarVertice(j+i*4, LetraSopa [i][j]);
+                z++;
+            }
+        }
+    }
+    
+    public void crearAristas (Grafo grafo){
+        //Crear Aristas (Prueba)
+        for (int i = 0; i <numFilas; i++){
+            for (int j = 0; j< numCol; j++){
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i-1][j-1]);}
+                catch (Exception e){}
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i-1][j]);}
+                catch (Exception e){}
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i-1][j+1]);}
+                catch (Exception e){}
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i][j-1]);}
+                catch (Exception e){}
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i][j+1]);}
+                catch (Exception e){}
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i+1][j-1]);}
+                catch (Exception e){}
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i+1][j]);}
+                catch (Exception e){}
+                try{grafo.NuevaArista(PosicioneSopa[i][j], PosicioneSopa[i+1][j+1]);}
+                catch (Exception e){}
+            }
+        }
+    }
+    
+    public String imprimirAdyacentes (int v){
+        //imprime Vertices Adyacentes de uno Solicitado
+        String cadena = "";
+        Vertice vertice = grafo.buscarVertice(v);
+        Arista arista = vertice.getAdyacencia().getFirst();
+        for (int i = 1; i < vertice.getAdyacencia().getNumAdy();i++){
+            cadena += Integer.toString(arista.getDestino())+", ";
+            arista = arista.getNext();
+        }
+        cadena += Integer.toString(arista.getDestino());
+        return cadena;
+    }
+    
+    public Pila buscarBFS (String palabra){
+        Busqueda buscarBFS = new Busqueda ();
+        return buscarBFS.buscarBFS(this.grafo, palabra.toUpperCase());
+    }
+    
+    public Pila buscarDFS (String palabra){
+        Busqueda_DFS buscarDFS = new Busqueda_DFS();
+        return buscarDFS.BuscarDFS(this.grafo, palabra);
+    }
+
+    public char[][] getLetraSopa() {
+        return LetraSopa;
+    }
+
+    public void setLetraSopa(char[][] LetraSopa) {
+        this.LetraSopa = LetraSopa;
+    }
+
+    public int[][] getPosicioneSopa() {
+        return PosicioneSopa;
+    }
+
+    public void setPosicioneSopa(int[][] PosicioneSopa) {
+        this.PosicioneSopa = PosicioneSopa;
+    }
+
+    public int getNumCol() {
+        return numCol;
+    }
+
+    public void setNumCol(int numCol) {
+        this.numCol = numCol;
+    }
+
+    public int getNumFilas() {
+        return numFilas;
+    }
+
+    public void setNumFilas(int numFilas) {
+        this.numFilas = numFilas;
+    }
+
+    public Grafo getGrafo() {
+        return grafo;
+    }
+
+    public void setGrafo(Grafo grafo) {
+        this.grafo = grafo;
+    }
+            
+        
+        
+}
+
+>>>>>>> Stashed changes
